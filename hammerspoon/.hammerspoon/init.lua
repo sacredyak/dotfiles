1a4fbb7266c39afd2124c278ad87354e012e9b77
zenobht <bharatjoshi.p@gmail.com>
2025-07-13 21:30:03 +1000

    hammerspoon: restructure and user hammerflow

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,40 +1,11 @@
-local log = hs.logger.new("init.lua", "debug")
-
--- -- Use Control+` to reload Hammerspoon config
--- hs.hotkey.bind({'alt','ctrl'}, '-', nil, function()
---   hs.reload()
--- end)
-
-keyUpDown = function(modifiers, key)
-  -- Un-comment & reload config to log each keystroke that we're triggering
-  -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
-
-  hs.eventtap.keyStroke(modifiers, key, 0)
+hs.loadSpoon("Hammerflow")
+spoon.Hammerflow.loadFirstValidTomlFile({
+  "home.toml",
+})
+-- optionally respect auto_reload setting in the toml config.
+if spoon.Hammerflow.auto_reload then
+  hs.loadSpoon("ReloadConfiguration")
+  -- set any paths for auto reload
+  -- spoon.ReloadConfiguration.watch_paths = {hs.configDir, "~/path/to/my/configs/"}
+  spoon.ReloadConfiguration:start()
 end
-
--- Subscribe to the necessary events on the given window filter such that the
--- given hotkey is enabled for windows that match the window filter and disabled
--- for windows that don't match the window filter.
---
--- windowFilter - An hs.window.filter object describing the windows for which
---                the hotkey should be enabled.
--- hotkey       - The hs.hotkey object to enable/disable.
---
--- Returns nothing.
-enableHotkeyForWindowsMatchingFilter = function(windowFilter, hotkey)
-  windowFilter:subscribe(hs.window.filter.windowFocused, function()
-    hotkey:enable()
-  end)
-
-  windowFilter:subscribe(hs.window.filter.windwUnfocused, function()
-    hotkey:disable()
-  end)
-end
-
--- require('windows')
--- require('border')
--- require('mappings')
-require("switcher")
--- require("hyper")
-
-hs.notify.new({ title = "Hammerspoon", informativeText = "Ready to rock 🤘" }):send()
